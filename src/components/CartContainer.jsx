c61710538055b526cbdaa2a82a633a4451ad1505
Frosenow <filip.potepa@gmail.com>
2024-02-03 21:01:16 +0100

    displaying caart items

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -3,5 +3,37 @@ import { useSelector } from "react-redux";
 
 export default function CartContainer() {
   const { cartItems, total, amount } = useSelector((store) => store.cart);
-  return <div>CartContainer</div>;
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>Your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>Your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn">Clear cart</button>
+      </footer>
+    </section>
+  );
 }
